48d700348bb27c2d5956eed22e8d5ec589ea92b9
Reisande <42556808+Reisande@users.noreply.github.com>
2018-08-21 15:26:44 -0500

    Add files via upload
    I redid a few problem using comprehensions instead of clunky iterables. My solution to 12 has gone down to 23 lines, and my solution to 1 has shrunk down to only 2.

--- Project Euler 12.py
+++ Project Euler 12.py
@@ -4,22 +4,18 @@ start_time = time.time()
 
 def divisors(divisors_check):
     root = int(divisors_check**.5) + 1
-    divisors_list = [1, divisors_check]
-    for i in range(2, root):
-        if divisors_check % i == 0:
-            inverse = divisors_check / i
-            divisors_list.append(i)
-            divisors_list.append(inverse)
-    return divisors_list  #check with is_prime/ eratosthenes_sieve
+    divisors_list = [(number, divisors_check/number) for number in range(1, root) if divisors_check % number == 0]
+    return len(divisors_list)*2
 
 
 def triangle():
     triangle_number = 0
     triangle_number_list = [0, 1, 3]
-    while len(divisors(triangle_number_list[-1])) < 500:
+    while divisors(triangle_number_list[-1]) < 500:
         triangle_number = 2*triangle_number_list[-1] - triangle_number_list[-2] + 1
         triangle_number_list.append(triangle_number)
     print(triangle_number)
+
 
 triangle()
 
